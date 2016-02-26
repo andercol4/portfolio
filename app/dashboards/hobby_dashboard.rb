67c0811f33a506570e141f214112a6c4afce15d0
Colin Anderson <andercol.4@gmail.com>
2016-02-26 09:33:03 -0700

    got started on a resume page added administrate gem

---
+++ app/dashboards/hobby_dashboard.rb
@@ -0,0 +1,57 @@
+require "administrate/base_dashboard"
+
+class HobbyDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    title: Field::String,
+    description: Field::Text,
+    rank: Field::Number,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :id,
+    :title,
+    :description,
+    :rank,
+  ]
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = [
+    :id,
+    :title,
+    :description,
+    :rank,
+    :created_at,
+    :updated_at,
+  ]
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :title,
+    :description,
+    :rank,
+  ]
+
+  # Overwrite this method to customize how hobbies are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(hobby)
+  #   "Hobby ##{hobby.id}"
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :hobbies
+
+    root to: "hobbies#index"
+  end
+
   root 'home#index'
+  get 'resume', to: 'home#resume', as: 'resume'
   resources :hobbies
 end
